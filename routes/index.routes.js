18af20216fa886c6b8ac72cebd262874bef6e75a
Diwakar <dwkrdahal@gmail.com.com>
2024-09-01 19:07:37 +0545

    index route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,22 +2,24 @@ import express from "express";
 const app = express();
 
 // routes
-import homeRoutes from "./home.routes.js"
-import authRoutes from "./auth.routes.js"
-import projectRoutes from "./project.routes.js"
-import userRoutes from "./user.routes.js"
-import messageRoutes from "./message.routes.js"
-import userPreferenceRoutes from "./user-preference.routes.js"
-import teamRoutes from "./team.routes.js"
-import serviceRoutes from "./service.routes.js"
+import homeRoutes from "./home.routes.js";
+import authRoutes from "./auth.routes.js";
+import projectRoutes from "./project.routes.js";
+import userRoutes from "./user.routes.js";
+import messageRoutes from "./message.routes.js";
+import userPreferenceRoutes from "./user-preference.routes.js";
+import teamRoutes from "./team.routes.js";
+import serviceRoutes from "./service.routes.js";
+import featureRoutes from "./feature.routes.js"
 
-app.use("/", homeRoutes)
-app.use("/auth", authRoutes)
-app.use("/project", projectRoutes)
-app.use("/user", userRoutes)
-app.use("/message", messageRoutes)
-app.use("/preference", userPreferenceRoutes)
-app.use("/team", teamRoutes)
-app.use("/service",serviceRoutes)
+app.use("/", homeRoutes);
+app.use("/auth", authRoutes);
+app.use("/project", projectRoutes);
+app.use("/user", userRoutes);
+app.use("/message", messageRoutes);
+app.use("/preference", userPreferenceRoutes);
+app.use("/team", teamRoutes);
+app.use("/service", serviceRoutes);
+app.use("/feature", featureRoutes);
 
 export default app;
